c2634520e9e2a4cc7b1fbfafb8e934566cd73c32
NielsVanheertum-BA <niels.vanheertum@bazandpoort.be>
2026-01-26 15:16:58 +0100

    Pause Menu done

--- eindproject/Scenes/main_menu.gd
+++ eindproject/Scenes/main_menu.gd
@@ -3,11 +3,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Engine.time_scale = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
+	pass
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
--- eindproject/Scenes/pause_menu.gd
+++ eindproject/Scenes/pause_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
-@onready var pause_menu: Control = $"."
+@onready var game: Node2D = $".."
+
+
+		
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 
 
 func _on_resume_pressed() -> void:
-	pause_menu.hide()
-
-func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	game.pauseMenu()
