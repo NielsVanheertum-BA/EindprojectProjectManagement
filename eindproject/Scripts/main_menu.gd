f54c18cfae95af6648b2b482a1511fb8c21252bb
NielsVanheertum-BA <niels.vanheertum@bazandpoort.be>
2026-02-06 14:16:12 +0100

    Changing keybinds werkt

--- eindproject/Scripts/main_menu.gd
+++ eindproject/Scripts/main_menu.gd
@@ -2,11 +2,13 @@ extends Control
 @onready var options_panel: Panel = $"Options Panel"
 @onready var main_buttons: VBoxContainer = $"Main Buttons"
 @onready var title: Label = $Title
+@onready var input_settings: Control = $InputSettings
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	options_panel.hide()
+	input_settings.hide()
 	Engine.time_scale = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,3 +29,13 @@ func _on_close_pressed() -> void:
 	options_panel.hide()
 	main_buttons.show()
 	title.show()
+	
+	
+
+func _on_change_keybinds_pressed() -> void:
+	input_settings.show()
+
+
+
+func _on_close_input_pressed() -> void:
+	input_settings.hide()
